9065346323d2e6a11ff9c91d7e496945ef74c611
Jeremias Alarcon <jeremias.alarcon@students.snow.edu>
2025-04-01 08:30:09 -0600

    We can do mermaidJS syntax

--- MazeWeb/Program.cs
+++ MazeWeb/Program.cs
@@ -1,10 +1,13 @@
 using MazeWeb.Components;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddMermaidJS(); // < -- Important Line <<<<<<<<<<
 
 var app = builder.Build();
 
