966165925680faa6285527504dbf3c5368222b45
Jeremias Alarcon <jeremias.alarcon@students.snow.edu>
2025-04-14 15:45:33 -0600

    Static instance of maze working

--- nifty_labyrinth/labyrinth/MazeGame.cs
+++ nifty_labyrinth/labyrinth/MazeGame.cs
@@ -2,14 +2,23 @@ namespace labyrinth;
 
 public class MazeGame
 {
-    public string MazeName {get; set;}
-    public MazeCell StartingLocation {get; private set;}
-    public MazeCell CurrentLocation {get; private set;}
+    public string MazeName { get; set; }
+    public MazeCell StartingLocation { get; private set; }
+    public MazeCell CurrentLocation { get; private set; }
 
     public void StartNewMaze(string mazeName)
     {
         MazeName = mazeName;
         StartingLocation = MazeUtilities.mazeFor(mazeName);
         CurrentLocation = StartingLocation;
+    }
+}
+
+public class MazeInstance : MazeGame
+{
+    public static MazeInstance Instance { get; } = new MazeInstance();
+    public MazeInstance()
+    {
+
     }
 }
