dd8f712beac6a79cafe64e20ffa4ccf786c7608c
Jeremias Alarcon <jeremias.alarcon@students.snow.edu>
2025-04-14 10:26:02 -0600

    blazor

--- nifty_labyrinth/Program.cs
+++ nifty_labyrinth/Program.cs
@@ -6,36 +6,36 @@ using labyrinth;
  * do NOT edit the value of YourName. Changing YourName will change which
  * maze you get back, which might invalidate all your hard work!
 */
-string YOUR_NAME = "TODO: Replace this string with your name.";
+var userName = "TODO: Replace this string with your name.";
 
 /* Change these constants to contain the paths out of your mazes. */
 string PATH_OUT_OF_MAZE = "WNWSSSNNNEEESSWWSNEESW";
 string PATH_OUT_OF_TWISTY_MAZE = "SSEENNEWE";
 
-MazeCell startLocation = MazeUtilities.mazeFor(YOUR_NAME);
+MazeCell startLocation = MazeUtilities.mazeFor(userName);
 
 /* Set a breakpoint here to explore your maze! */
 
 if (MazeUtilities.isPathToFreedom(startLocation, PATH_OUT_OF_MAZE))
 {
-Console.WriteLine("Congratulations! You've found a way out of your labyrinth.");
+Console.WriteLine($"Congratulations! {userName} You've found a way out of your labyrinth.");
 }
 else
 {
-Console.WriteLine("Sorry, but you're still stuck in your labyrinth.");
+Console.WriteLine($"Sorry {userName}, but you're still stuck in your labyrinth.");
 }
 
 
-MazeCell twistyStartLocation = MazeUtilities.twistyMazeFor(YOUR_NAME);
+MazeCell twistyStartLocation = MazeUtilities.twistyMazeFor(userName);
 
 /* Set a breakpoint here to explore your twisty maze! */
 
 if (MazeUtilities.isPathToFreedom(twistyStartLocation, PATH_OUT_OF_TWISTY_MAZE))
 {
-Console.WriteLine("Congratulations! You've found a way out of your twisty labyrinth.");
+Console.WriteLine($"Congratulations! {userName} You've found a way out of your twisty labyrinth.");
 }
 else
 {
-Console.WriteLine("Sorry, but you're still stuck in your twisty labyrinth.");
+Console.WriteLine($"Sorry {userName}, but you're still stuck in your twisty labyrinth.");
 }
 
