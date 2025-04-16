1c26160b72b512031c4fba9c006a674628a83781
Jeremias Alarcon <jeremias.alarcon@students.snow.edu>
2025-04-16 11:23:46 -0600

    UpdateLeaderboard started, check if it works

--- nifty_labyrinth/labyrinth/MazeGame.cs
+++ nifty_labyrinth/labyrinth/MazeGame.cs
@@ -8,20 +8,26 @@ public class MazeGame
     public string MazeName { get; set; }
     public MazeCell StartingLocation { get; private set; }
 
+    public Dictionary<string, Player> Players { get; } = new();
+    public Player? Winner { get; private set; }
+
     public void StartNewMaze(string mazeName)
     {
         MazeName = mazeName;
         StartingLocation = MazeUtilities.mazeFor(mazeName);
     }
-    
+
     public void StartNewTwistyMaze(string mazeName)
     {
         MazeName = mazeName;
         StartingLocation = MazeUtilities.twistyMazeFor(mazeName);
     }
 
+    //Events
+    public event Action UpdateLeaderboard;
+
     //Try to understand this -
-    public (MazeCell, string) Movement(string direction, MazeCell location)
+    public (MazeCell, string) Movement(string direction, MazeCell location, string playerName)
     {
         try
         {
@@ -38,6 +44,18 @@ public class MazeGame
             if (nextCell != null)
             {
                 location = nextCell;
+
+                Players[playerName].Location = location;
+                if (location.WhatsHere != Item.Nothing && !Players[playerName].Items.Contains(location.WhatsHere))
+                {
+                    Players[playerName].Items.Add(location.WhatsHere);
+                    if (Players[playerName].Items.Count >= 3 && Winner == null)
+                    {
+                        Winner = Players[playerName];
+                    }
+                }
+                UpdateLeaderboard?.Invoke();
+
                 return (location, $"You moved {direction} to cell {location.Id}.");
             }
         }
---
+++ nifty_labyrinth/labyrinth/Player.cs
@@ -0,0 +1,9 @@
+namespace labyrinth;
+
+public class Player
+{
+    public string Name {get; set;}
+    public MazeCell Location {get; set;}
+    public List<Item> Items {get;} = new();
+
+}
